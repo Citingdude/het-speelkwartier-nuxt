65984195e70635302b68291e2245dff9b5e62b5c
Glenn <glenn.reumers@hotmail.com>
2022-01-13 21:53:12 +0100

    Create ArrowButton component

---
+++ components/buttons/ArrowButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtLink class="flex items-center" :to="link">
+    <p class="text-accent sm:text-lg">Shop</p>
+    <IconsArrowIcon class="w-8 h-8" />
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+    props: {
+        link: {
+            type: String,
+            default: "/"
+        },
+    },
+};
+</script>
+
+<style>
+</style>
