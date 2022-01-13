e1888f13dd2bfe0ffa6a92c28bee10f145d59a66
Glenn <glenn.reumers@hotmail.com>
2022-01-13 21:16:46 +0100

    Extend tailwind colors with champagne and goldenrod

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
       colors: {
         'accent': '#ee3661',
         'dark': '#0c1b33',
-        'light': '#fcfbf9'
+        'light': '#fcfbf9',
+        'goldenrod': '#f7fbd0',
+        'champagne': '#fce7d7',
       }
     },
   },
