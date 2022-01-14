af59e85576e848860309d4af2e85de2226375823
Glenn <glenn.reumers@hotmail.com>
2022-01-14 22:01:37 +0100

    Create PopularSection

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="p-home">
     <PagesHomeHeroSection />
+    <PagesHomePopularSection :featuredProducts="featuredProducts" />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+    async asyncData({ $axios }) {
+    const featuredProducts = await $axios.$get("https://hetspeelkwartier.be/wp-json/wc/v3/products?featured=true", {
+        withCredentials: true,
+        auth: {
+            username: 'ck_3aa0463ce2045297fd16429950da23d277376d51',
+            password: 'cs_677b67c7bf26dc4c481e25c36b2bf60e640cb000'
+        }
+    });
+
+    return { featuredProducts };
+  },
+}
 </script>
