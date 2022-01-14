f27c62df5cf32f1f70efdf35317aec1d8515a231
Glenn <glenn.reumers@hotmail.com>
2022-01-14 22:01:20 +0100

    Create ProductCard component

---
+++ components/products/ProductCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img
+      :src="image.src"
+      alt=""
+      class="mb-4 rounded-2xl"
+    />
+
+    <h3 class="text-2xl mb-4 font-semibold">{{ title }}</h3>
+
+    <ButtonsArrowButton />
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: 'Product title'
+        },
+        image: {
+            type: Object,
+            default: {}
+        }
+    },
+};
+</script>
+
+<style>
+</style>
