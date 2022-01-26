59eb30ebc7306f4ec065c816997018b498f66696
Glenn <glenn.reumers@hotmail.com>
2022-01-26 21:07:18 +0100

    Set dimensions for product image

--- components/products/ProductCard.vue
+++ components/products/ProductCard.vue
@@ -3,7 +3,7 @@
     <img
       :src="image.src"
       alt=""
-      class="mb-4 rounded-2xl"
+      class="mb-4 rounded-2xl max-h-72 h-full w-full object-cover"
     />
 
     <h3 class="text-2xl mb-4 font-semibold">{{ title }}</h3>
