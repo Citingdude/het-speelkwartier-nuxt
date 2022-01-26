2eda347f259238c0bb14299e90375e4b8dcb731c
Glenn <glenn.reumers@hotmail.com>
2022-01-26 21:57:43 +0100

    Create ShopArchive

---
+++ components/shop/ShopArchive.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container mx-auto">
+    <div class="grid sm:grid-cols-2 gap-8">
+      <ProductsProductCard
+        v-for="product in products"
+        :key="product.id"
+        :title="product.name"
+        :image="product.images[0]"
+        :id="product.id"
+        :category="product.categories[0].name"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        products: {
+            type: Array,
+            default: []
+        },
+    },
+};
+</script>
+
+<style>
+</style>
