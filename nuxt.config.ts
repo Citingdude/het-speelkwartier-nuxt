66a8f27e4d098d3cd54608bdc3ada7937942c7f1
Glenn <glenn.reumers@appwise.be>
2022-11-13 15:35:30 +0100

    Add config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    runtimeConfig: {
+    clientKey: '',
+    secretKey: '',
+    public: {
+      apiBase: ''
+    }
+  },
   typescript: {
     shim: false,
   },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
 })
