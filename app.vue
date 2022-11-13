c306ba90327fe278ee3205fe9a1a0f4ad4b94b31
Glenn <glenn.reumers@appwise.be>
2022-11-13 14:29:07 +0100

    Install nunito font

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <Test />
+  <div class="font-bold">
+    TEST
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,5 @@ export default defineNuxtConfig({
     shim: false,
   },
   modules: ['@nuxtjs/tailwindcss'],
+  css: ['~/assets/css/main.css']
 })
