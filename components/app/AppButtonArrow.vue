e0ae5ba68e11065e8fe554e506398ef5c15201c8
Glenn <glenn.reumers@appwise.be>
2024-01-07 10:32:10 +0100

    Add support for button

--- components/app/AppButtonArrow.vue
+++ components/app/AppButtonArrow.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
-const props = defineProps({
-  to: String,
+const props = defineProps<{
+  to?: string
+  isButton?: boolean
+}>()
+
+const component = computed(() => {
+  if (props.isButton)
+    return 'button'
+  else
+    return resolveComponent('NuxtLink')
 })
 </script>
 
 <template>
-  <NuxtLink class="flex items-center" :to="props.to">
+  <component :is="component" class="flex items-center" :to="props.to">
     <p class="text-accent sm:text-lg font-bold">
       Shop
     </p>
     <IconsArrow class="w-8 h-8" />
-  </NuxtLink>
+  </component>
 </template>
 
 <style>
