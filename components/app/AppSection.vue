a9b9654ab98d33268dfe2b685ea620458203d5a3
Glenn <glenn.reumers@appwise.be>
2024-01-28 10:01:28 +0100

    Craete section component

---
+++ components/app/AppSection.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  disablePadding?: boolean
+}>()
+</script>
+
+<template>
+  <section
+    :class="{
+      'py-10 lg:py-20': !disablePadding,
+    }"
+  >
+    <div class="container">
+      <slot />
+    </div>
+  </section>
+</template>
